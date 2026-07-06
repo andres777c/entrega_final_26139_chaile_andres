148ce3719c5349b9b5471ebceb6d7670f5c45542
andres777c <andresfiunju19@gmail.com>
2026-07-06 01:27:03 -0300

    Initial commit

---
+++ src/main/java/com/talento/articulo_api/ArticuloApiApplication.java
@@ -0,0 +1,12 @@
+package com.talento.articulo_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class ArticuloApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticuloApiApplication.class, args);
+	}
+
+}
